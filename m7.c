d1c3f83fcceb72147ff9f6183b48c4ac5b2819f3
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-02-20 16:35:06 +0530

    Create m7.c

---
+++ m7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+int n;
+printf("Enter a number:");
+scanf("%d",&n);
+if(isdigit(n))
+{
+print("It is a num:%d");
+}
+else
+{
+printf("It is not a Number);
+}
+return 0;
+}
